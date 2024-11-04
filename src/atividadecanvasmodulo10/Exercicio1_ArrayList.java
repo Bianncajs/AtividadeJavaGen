001c6e92e0ee7fca908ecd5ea2b70381b6273b92
Bianncajs <biancajesus.silva@outlook.com>
2024-11-04 15:58:14 -0300

    Nova atividade1

--- src/atividadecanvasmodulo10/Exercicio1_ArrayList.java
+++ src/atividadecanvasmodulo10/Exercicio1_ArrayList.java
@@ -12,28 +12,24 @@ public class Exercicio1_ArrayList {
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Digite 5 cores:");
-
 		for (int i = 0; i < 5; i++) {
 			System.out.print("Cor " + (i + 1) + ": ");
 			String cor = scanner.nextLine();
 			cores.add(cor);
-
 		}
 
-		System.out.println("\nCores adicionadas:");
+		scanner.close();
+
+		System.out.println("\nListar todas as cores:");
 		for (String cor : cores) {
 			System.out.println(cor);
 		}
 
 		Collections.sort(cores);
 
-		System.out.println("\nCores ordenadas em ordem crescente:");
+		System.out.println("\nOrdenar as cores:");
 		for (String cor : cores) {
 			System.out.println(cor);
-
-			scanner.close();
 		}
-
 	}
-
 }
